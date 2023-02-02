e737708793cc40d230dd164500aea48fa8289353
Kurt Harriger <kurtharr@amazon.com>
2023-02-02 23:07:19 +0000

    Add nrepl middleware and allow port to be configured

--- src/clojure/spring_repl/nrepl.clj
+++ src/clojure/spring_repl/nrepl.clj
@@ -1,10 +1,13 @@
 (ns spring-repl.nrepl
   (:require
     [nrepl.server :refer [start-server stop-server]]
+    [cider.nrepl :refer [cider-nrepl-handler]]
     [clojure.tools.logging :as log]))
 
 (def repl-server (atom nil))
 
+(defn spring-repl-port [] (if-let [p (System/getProperty "springReplPort")] (Integer/parseInt p) 8000))
+
 (defn start-repl []
-  (log/info "Starting nrepl server")
-  (reset! repl-server (start-server :port 8000 :bind "0.0.0.0")))
+  (log/info "Starting nrepl server on " (spring-repl-port))
+  (reset! repl-server (start-server :port (spring-repl-port) :bind "0.0.0.0" :handler cider-nrepl-handler)))
